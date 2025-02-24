b42c786c9b555199bd03d6f52034eea96247c4a6
Misha <Mishap635@gmail.com>
2025-02-24 21:04:42 +0200

    туц

--- func.t.js
+++ func.t.js
@@ -16,21 +16,3 @@ console.log(result2)
 const pow = function (number) {
     return number ** 2;
   }
-
-
-  const pow2 = (number2) =>  number ** 2;
-
-  function pow3 (number3){
-    const sum2 = number3 **2
-    return sum2
-  }
-
-
-  //параметр
-  function greet(name = 'guest') {
-    console.log(`Hello, ${name}`)
-  }
-
-  //аргумент
-  greet('Anna')
-  greet()
